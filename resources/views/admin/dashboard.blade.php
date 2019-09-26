3a5c5282b1547743f9f5a7a3ad92a725d3edccdf
Diwa Perkasa <diwa.perkasa@gmail.com>
2019-09-26 17:14:13 +0700

    Update dashboard.blade.php
    remove container-fluid

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,27 +8,23 @@
 @section('content')
     <div class="card">
         <div class="card-body">
-            <div class="row">
-                <div class="container-fluid">
-                    <div class="form-group">
-                        <button class="btn btn-primary">Halo</button>
-                    </div>
-                    <div class="form-group">
-                        <label for="staticEmail">Email</label>
-                        <input type="text" class="form-control" id="staticEmail" value="email@example.com">
-                    </div>
-                    <div class="form-group">
-                        <label for="select-dol">Example select</label>
-                        <select class="form-control" id="select-dol">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                    </div>
+                <div class="form-group">
+                    <button class="btn btn-primary">Halo</button>
                 </div>
-            </div>
+                <div class="form-group">
+                    <label for="staticEmail">Email</label>
+                    <input type="text" class="form-control" id="staticEmail" value="email@example.com">
+                </div>
+                <div class="form-group">
+                    <label for="select-dol">Example select</label>
+                    <select class="form-control" id="select-dol">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
         </div>
     </div>
 @endsection
